567c70efdd1baf72e02f32f5aa41fcfee643bd26
starcrystal78 <star.crystal78@gmail.com>
2021-11-01 15:05:25 +0545

    admin.py file changes with list and filter

--- Surface/admin.py
+++ Surface/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import WorkDetail
 
 # Register your models here.
+
+
 @admin.register(WorkDetail)
 class WorkDetailAdmin(admin.ModelAdmin):
-	pass
+    list_display = ('project', "project_type", 'duration', "is_complete")
+    list_filter = ('is_complete', )
